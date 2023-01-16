baf514e3dd573ab47427054f2ab2f4536f70e1c2
motik <motik@10.0.0.61>
2023-01-15 23:30:23 -0500

    commit555

--- src/Branch1/Demo1.java
+++ src/Branch1/Demo1.java
@@ -2,10 +2,8 @@ package Branch1;
 
 public class Demo1 {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("Hello");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
 	}
 
 }
